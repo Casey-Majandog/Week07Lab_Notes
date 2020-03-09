65e4f7853e5b69fdc6dbf521e77f68eaac56f369
casey-majandog <casey.majandog@hotmail.com>
2020-03-09 12:53:22 -0600

    Added methods

--- src/java/dataaccess/NoteDB.java
+++ src/java/dataaccess/NoteDB.java
@@ -5,10 +5,87 @@
  */
 package dataaccess;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import models.Note;
+
 /**
  *
  * @author 813793
  */
 public class NoteDB {
+    
+    public int insert(Note note){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.persist(note);
+            et.commit();
+            return 1;
+        } catch (Exception e) {
+            et.rollback();
+            throw e;
+        } finally {
+
+            em.close();
+        }
+    }
+    
+    public int update(Note note){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.merge(note);
+            et.commit();
+            return 1;
+        } catch (Exception e) {
+            et.rollback();
+            throw e;
+        } finally {
+
+            em.close();
+        }
+    }
+    
+    public List<Note> getAll() {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            List<Note> notes = em.createNamedQuery("Note.findAll", Note.class).getResultList();
+            return notes;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Note get(int noteId) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            Note note = em.find(Note.class, noteId);
+            return note;
+        } finally {
+            em.close();
+        }
+    }
+    
+    public int delete(Note note){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+            em.remove(em.merge(note));
+            et.commit();
+            return 1;
+        } catch (Exception e) {
+            et.rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
+    
+    
     
 }
